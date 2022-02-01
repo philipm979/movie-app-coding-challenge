ad80da934629f3489845e16cf48292551b2169c1
Philip Mcloughlin <philipmcloughlin@Philips-MacBook.local>
2022-02-01 11:17:41 +0000

    delete some comments

--- movie-app/src/containers/worker.js
+++ movie-app/src/containers/worker.js
@@ -1,20 +1,4 @@
 
-// export default function worker () {
-//     // eslint-disable-next-line no-restricted-globals
-//     self.onmessage = (e) => {
-//         const searchArray = e.data.map((movie)=>{
-//             const firstPart = movie.Year.split('-')[0];
-//             return {...movie, Year: firstPart}});
-        
-//     // const response = searchArray.reduce((r,a) => {
-//     //                     r[a.Year] = r[a.Year] || [];
-//     //                     r[a.Year].push(a);
-//     //                     return r;
-//     //                 }, Object.create(null))
-//       // // eslint-disable-next-line no-restricted-globals
-//     self.postMessage(searchArray);
-
-//               }
 
 export default function worker () {
     // eslint-disable-next-line no-restricted-globals
